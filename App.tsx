049fd51d339f710a22e0c8769d6314a608b35dcc
Mikita Hlushkou <mikita.hlushkou@gmail.com>
2022-07-31 19:12:53 +0300

    add safe area to App.tsx

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import { SafeAreaView } from 'react-native';
 
 const App = () => {
   const isLoadingComplete = useCachedResources();
@@ -14,8 +15,10 @@ const App = () => {
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
+        <SafeAreaView style={{ flex: 1, backgroundColor: 'black' }}>
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+        </SafeAreaView>
       </SafeAreaProvider>
     );
   }
